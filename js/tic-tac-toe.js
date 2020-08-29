d9338f61b24473f27c3a63bf3b205e8ec163bb88
vaulstein <vaulstein@gmail.com>
2020-08-29 12:32:20 +0530

    Added boilerplate

---
+++ js/tic-tac-toe.js
@@ -0,0 +1,37 @@
+// TIC TAC TOE
+const tic_tac_toe = {
+
+  // ATTRIBUTES
+
+  // FUNCTIONS
+  init(container) {
+  },
+
+  make_play(position) {
+
+  },
+
+  stylize_winner_sequence(winner_sequence, currentSymbol) {
+
+  },
+
+  check_winning_sequences(symbol) {
+
+  },
+
+  game_is_over() {
+  },
+
+  is_game_over() {
+  },
+
+  start() {
+
+  },
+
+  restart() {
+  },
+
+  draw() {
+  },
+};
